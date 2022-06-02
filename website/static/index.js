4c3dca6fa54feb4ceadfa451aa3645a94b283abe
tom <tomkingori66@gmail.com>
2022-06-02 10:15:02 +0300

    final commit

--- website/static/index.js
+++ website/static/index.js
@@ -1,17 +1,4 @@
-// const task_input_el = document.getElementById('task-input')
-// const task_edit_el = document.getElementById('task-edit');
 
-// task_edit_el.addEventListener('click', () => {
-//     if (task_edit_el.innerText.toLowerCase() == "edit") {
-//             task_input_el.removeAttribute("readonly");
-//             task_input_el.focus();
-//             task_edit_el.innerText = "Save";
-//     }
-//     else {
-//         task_input_el.setAttribute("readonly", "readonly");
-//         task_edit_el.innerText = "Edit";
-//     }
-// });
 
 function deleteNote(noteId) {
     fetch('/delete-note', {
